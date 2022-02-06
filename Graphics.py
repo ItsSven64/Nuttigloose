3a4d68d3da8ed25ff068b723fb025ad8e8cf9b9e
Sven de Wit <svendewit1707@gmail.com>
2022-02-06 14:09:07 +0100

    Functies toegevoegd, bezig met match-case functies

--- Graphics.py
+++ Graphics.py
@@ -1,24 +1,39 @@
+from re import *
+
 from tkinter import *
 from tkinter import ttk
 
-def Init():
-    global root
+
+def init():
     global frm
+    global root
     root = Tk()
     root.geometry("1000x1000")
     root.resizable(False, False)
     frm = ttk.Frame(root, padding=10)
     frm.grid()
+
+
 def Button(Text, column, row):
     ttk.Button(frm, text=Text, command=lambda m=Text: pressed(m)).grid(column=column, row=row)
 
+
 def DisplayText(Text, column, row):
-    ttk.Label(frm, text=Text).grid(column=column, row=row)
+    global Label
+    Label = ttk.Label(frm, text=Text).grid(column=column, row=row)
+
 
 def pressed(Message):
+    print(Message)
+
     match Message:
         case "Quit":
+            Label.destroy()
             DisplayText("Nope", 1, 1)
+        case "Empty":
+            pass
 
-Init()
+
+init()
+Button("Quit", 1, 1)
 root.mainloop()
