0134bc8e8e54bd674ec54b033ac173b7299509cb
Sven de Wit <svendewit1707@gmail.com>
2022-02-07 08:08:51 +0100

    Bestand met alle functies voor graphics, en een klein test programma

--- Graphics.py
+++ Graphics.py
@@ -1,5 +1,5 @@
-from re import *
-
+from time import *
+imor
 from tkinter import *
 from tkinter import ttk
 
@@ -8,32 +8,42 @@ def init():
     global frm
     global root
     root = Tk()
-    root.geometry("1000x1000")
+    root.geometry("1000x1001")
     root.resizable(False, False)
     frm = ttk.Frame(root, padding=10)
     frm.grid()
 
 
 def Button(Text, column, row):
-    ttk.Button(frm, text=Text, command=lambda m=Text: pressed(m)).grid(column=column, row=row)
-
+    BTNid = [ttk.Button(frm, text=Text, command=on_click(Text)).grid(column=column, row=row), Text]
+    return BTNid
 
 def DisplayText(Text, column, row):
-    global Label
-    Label = ttk.Label(frm, text=Text).grid(column=column, row=row)
+    TXTid = [ttk.Label(frm, text=Text).grid(column=column, row=row), Text]
+    return TXTid
 
 
-def pressed(Message):
-    print(Message)
+def on_click(BTNid):
+    global stage
+    match BTNid:
+        case "Start loading!":
+            stage = 1
 
-    match Message:
-        case "Quit":
-            Label.destroy()
-            DisplayText("Nope", 1, 1)
-        case "Empty":
-            pass
+def DeClick():
+    clicked = False
 
 
+
+stage = 0
 init()
-Button("Quit", 1, 1)
-root.mainloop()
+Button("Start loading", 1, 1)
+Button("Start loading!", 50, 100)
+DisplayText("Welcome to Nuttigloos!", 1, 1)
+while True:
+    match stage:
+        case 0:
+
+        case 1:
+            print("Click!")
+            stage = 0
+    root.mainloop()
