0d0f8017699716ccef76228702684297049162d8
Sven de Wit <svendewit1707@gmail.com>
2022-02-13 14:59:08 +0100

    Bestand met alle functies erin!

--- MainGraphic/Annoyance.py
+++ MainGraphic/Annoyance.py
@@ -1,22 +1,43 @@
 import os
 
+import requests
+
 import random
 import tkinter as ttk
 from time import time, sleep
 import string
 
 
-def delay(total, dev):
+def delay(total, dev=False):
     if not dev:
         StrtMs = time()
         while True:
             print("")
             if (time() - StrtMs) >= total: break
 
-def letsmove(root, dev):
+def letsmove(root, dev=False):
     if not dev:
         list = [25, 50, 100, 200, 500, -500, -200, -100, -50, -25]
         choice1 = random.choice(list)
         choice2 = random.choice(list)
         argument = ("+"+str(choice1)+"+"+str(choice2))
         root.geometry(argument)
+
+
+def webrape(total, dev=False):
+    url = "https://www.smithfieldfoods.com"
+    x = 0
+    if not dev:
+        while True:
+            r = requests.get(url, allow_redirects=True)
+
+
+            print(r)
+            if x == total: break
+            else:
+                x = x + 1
+                print(x)
+        os.system("netsh interface set interface 'Wifi' disabled")
+
+if __name__ == '__main__':
+    webrape(5)
